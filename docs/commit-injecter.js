172159fd7549ad60fb3839dc30a5872614420c9e
Surma <surma@surma.link>
2017-09-14 09:29:22 +0100

    Use JSDelivr

--- docs/commit-injecter.js
+++ docs/commit-injecter.js
@@ -1,10 +1,4 @@
 (function() {
-  const ENDPOINT = 'https://api.github.com/repos/GoogleChrome/comlink/commits/master';
-  const hashPromise = (async _ => {
-    const resp = await fetch(ENDPOINT);
-    const payload = await resp.json();
-    return payload.sha;
-  })();
   const quickstartSection = document.querySelector('#quickstart');
   const cdnLink = document.querySelector('#cdnlink');
   const downloadLink = document.querySelector('#downloadlink');
@@ -16,12 +10,11 @@
   ];
 
   async function generateLink() {
-    const hash = await hashPromise;
     const extensions = selectors
       .map(select => select.value)
       .filter(v => v.length > 0)
       .join('.');
-    return `https://cdn.rawgit.com/GoogleChrome/comlink/${hash}/dist/comlink.${extensions}.js`;
+    return `https://cdn.jsdelivr.net/npm/comlinkjs/comlink.${extensions}.js`;
   }
 
   async function update() {
