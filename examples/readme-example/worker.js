7f995a08a38ed58a12df343c8489bbe2e15bd7be
Surma <surma@surma.link>
2017-09-16 17:23:10 +0200

    Add Presentation API example

--- examples/readme-example/worker.js
+++ examples/readme-example/worker.js
@@ -11,7 +11,7 @@
  * limitations under the License.
  */
 
-importScripts('../../dist/comlink.global.js');
+importScripts('https://cdn.jsdelivr.net/npm/comlinkjs/comlink.global.min.js');
 
 class App {
   constructor() {
