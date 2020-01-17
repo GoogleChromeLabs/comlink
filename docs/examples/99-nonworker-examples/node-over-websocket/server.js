502e7ef074e2754c05b38ac30b6f07280cd2e952
Surma <surma@surma.link>
2020-01-17 16:10:49 +0000

    Make it work over WebSockets

--- docs/examples/99-nonworker-examples/node-over-websocket/server.js
+++ docs/examples/99-nonworker-examples/node-over-websocket/server.js
@@ -1,62 +1,30 @@
-const { MessageChannel, MessagePort } = require("worker_threads");
-globalThis.MessageChannel = MessageChannel;
-globalThis.MessagePort = MessagePort;
-const Comlink = require("../../../../dist/umd/comlink.js");
-const { wrap } = require("../../../../dist/umd/string-channel.experimental.js");
-const nodeEndpoint = require("../../../../dist/umd/node-adapter.js");
-const { join } = require("path");
-const url = require("url");
-const http = require("http");
-const { promises: fsp } = require("fs");
+const URL = require("url");
+const Express = require("express");
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ noServer: true });
+const http = require("http");
+const { join } = require("path");
+
+const Comlink = require("../../../../dist/umd/comlink.js");
+const { websocketEndpoint } = require("./websocket-adapter.js");
+
+const app = Express();
+app.use("/dist/", Express.static("../../../../dist/"));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "./index.html"));
+});
+
+const api = {
+  counter: 0,
+  increase(delta = 1) {
+    this.counter += delta;
+    console.log(`Counter set to ${this.counter}`);
+  }
+};
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server, path: "/ws" });
 wss.on("connection", ws => {
-  const obj = {
-    hello() {
-      console.log("What?");
-      return "Done";
-    }
-  };
-  Comlink.expose(
-    obj,
-    nodeEndpoint(
-      wrap({
-        addMessageListener(f) {
-          ws.addEventListener("message", ev => f(ev.data));
-        },
-        send(msg) {
-          ws.send(msg);
-        }
-      })
-    )
-  );
+  Comlink.expose(api, websocketEndpoint(ws));
 });
 
-const server = new http.Server();
-server.on("upgrade", async (req, res, head) => {
-  const reqUrl = url.parse(req.url);
-  if (reqUrl.pathname !== "/ws") {
-    res.statusCode = 400;
-    res.end("No websocket at this path");
-  }
-  wss.handleUpgrade(req, res, head, socket => {
-    wss.emit("connection", socket, req);
-  });
-});
-
-server.on("request", async (req, res) => {
-  res.setHeader("Cache-Control", "max-age=0");
-  const reqUrl = url.parse(req.url);
-  if (reqUrl.pathname === "/ws") {
-    return;
-  }
-  if (reqUrl.pathname.startsWith("/dist")) {
-    const fullPath = join("../../../../", reqUrl.pathname);
-    res.setHeader("Content-Type", "text/javascript");
-    res.end(await fsp.readFile(fullPath));
-    return;
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.end(await fsp.readFile("index.html"));
-});
-server.listen(8080);
+server.listen("8080");
