3118afcd24ef67aeebf608af22aa1131b8284100
Chris Kruining <chris@kruining.eu>
2020-11-13 14:55:49 +0100

    proper UUID implementetion
    by curtesy of https://gist.github.com/jed/982883
    
    current implementation is not safe against collisions

--- src/comlink.ts
+++ src/comlink.ts
@@ -544,8 +544,5 @@ function requestResponseMessage(
 }
 
 function generateUUID(): string {
-  return new Array(4)
-    .fill(0)
-    .map(() => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16))
-    .join("-");
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
 }
