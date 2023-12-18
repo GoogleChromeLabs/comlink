432fff79d9fd6ce9e807cef9bffadd9fc0f7fc29
Dave Baskin <dfbaskin@gmail.com>
2023-12-18 10:57:55 -0500

    Adding serve package for test fixtures.

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npx serve -p 3000 tests/fixtures',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: true,
+  },
 });
